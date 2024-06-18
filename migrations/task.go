1180cb74fbb78a1dfd32db71bee97463516a9905
VLADislav314 <vladnety134@gmail.com>
2024-06-18 19:11:10 +0300

    da.

---
+++ migrations/task.go
@@ -0,0 +1,32 @@
+package db
+
+// import "fmt"
+
+type Task struct {
+    TaskID         int
+    TaskName       string
+    TaskDescription string
+    SolutionCode   string
+    TestCases      interface{}
+}
+
+// GetAllTasks retrieves all tasks from the database
+func GetAllTasks() ([]Task, error) {
+    var tasks []Task
+    rows, err := DB.Query("SELECT * FROM tasks")
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+        var task Task
+        err := rows.Scan(&task.TaskID, &task.TaskName, &task.TaskDescription, &task.SolutionCode, &task.TestCases)
+        if err != nil {
+            return nil, err
+        }
+        tasks = append(tasks, task)
+    }
+
+    return tasks, nil
+}
